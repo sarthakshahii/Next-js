bfc592083dc79ba8e76d8e82a35115332bbaf7e5
sarthakshahii <sarthaksshahi@gmail.com>
2025-08-16 16:14:01 +0530

    First commit - moved to my repo

---
+++ data-fetching-demo/prisma/migrations/20250816063730_init/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `price` on the `Product` table. The data in that column could be lost. The data in that column will be cast from `Int` to `Float`.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "description" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_Product" ("description", "id", "price", "title") SELECT "description", "id", "price", "title" FROM "Product";
+DROP TABLE "Product";
+ALTER TABLE "new_Product" RENAME TO "Product";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
